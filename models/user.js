5eb838c9d5195397383814c46c79f133c6ddebab
GAURAV KUMAR <153052732+grv1831@users.noreply.github.com>
2025-06-29 14:03:09 +0530

    Update user.js

--- models/user.js
+++ models/user.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
-mongoose.connect("mongodb+srv://1831gkumar:grv%40123@cluster0.mongodb.net/crud", {
+mongoose.connect("mongodb+srv://1831gkumar:grv%40123@cluster0.l8fn9i7.mongodb.net/crudDB?retryWrites=true&w=majority&appName=Cluster0
+", {
   useNewUrlParser: true,
   useUnifiedTopology: true
 });
