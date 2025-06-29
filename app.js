e8b8405e5f73767ef637708c1d38825cccd8c9e1
GAURAV KUMAR <153052732+grv1831@users.noreply.github.com>
2025-06-29 21:25:00 +0530

    Update app.js

--- app.js
+++ app.js
@@ -1,13 +1,14 @@
 require('dotenv').config();
 const mongoose = require('mongoose');
 
-mongoose.connect(process.env.MONGODB_URI, {
-  useUnifiedTopology: true
-}).then(() => {
-  console.log("MongoDB connected!");
-}).catch(err => {
-  console.error("MongoDB connection error:", err);
-});
+mongoose.connect(process.env.MONGODB_URI)
+  .then(() => {
+    console.log("MongoDB connected!");
+  })
+  .catch((err) => {
+    console.error("MongoDB connection error:", err);
+  });
+
 
 
 const express = require("express");
